a2071d4aeada4f33e94fb5220fac5c8c31100ba9
ds8086 <94914992+ds8086@users.noreply.github.com>
2026-01-08 16:03:11 -0800

    2025.01.08

---
+++ IIS/Get-WebsiteLogDirectory.ps1
@@ -0,0 +1,66 @@
+﻿Function Get-WebsiteLogDirectory {
+<#
+.SYNOPSIS
+Retrieve website(s) and their corresponding log directory.
+
+.DESCRIPTION
+Retrieve website(s) and their corresponding log directory.
+
+.NOTES
+Author: 
+    DS
+Notes:
+    Revision 02
+Revision:
+    V01: 2022.05.23 by DS :: First revision.
+    V02: 2026.01.08 by DS :: Revamp for GitHub.
+Call From:
+    Windows PowerShell v5.1 or higher with WebAdministration module
+
+.INPUTS
+None
+
+.OUTPUTS
+None
+
+.EXAMPLE
+Get-WebsiteLogDirectory
+Retrieve website(s) and their corresponding log directory.
+#>
+
+[CmdletBinding()]
+param ()
+
+# Define and import required modules
+$RequiredModules = "WebAdministration"
+foreach ($rm in $RequiredModules) {
+    try {
+        if (!(Get-Module -Name $rm)) {
+            Import-Module -Name $rm -ErrorAction Stop
+        }
+    }
+    catch {
+        throw
+    }
+}
+
+$Websites = Get-Website
+if ($Websites) {
+    $Websites | Select-Object @{N="PSComputerName";E={$env:COMPUTERNAME}},
+        name,
+        id,
+        state,
+        physicalPath,
+        @{N="logs";E={($_.logfile.directory).Replace('%SystemDrive%',$env:SystemDrive) + "\w3svc" + $_.id}}
+}
+else {
+    Write-Warning "No websites found on $env:COMPUTERNAME"
+    "" | Select-Object @{N="PSComputerName";E={$env:COMPUTERNAME}},
+        @{N="name";E={[string]::new("No websites")}},    
+        @{N="id";E={[string]::new("No websites")}},
+        @{N="state";E={[string]::new("No websites")}},
+        @{N="physicalPath";E={[string]::new("No websites")}},
+        @{N="logs";E={[string]::new("No websites")}}
+}
+
+}
---
+++ Windows/Invoke-DiskSetup.ps1
@@ -0,0 +1,109 @@
+﻿Function Invoke-QuickDiskSetup {
+<#
+.SYNOPSIS
+Invokes quick disk setup.
+
+.DESCRIPTION
+Invokes quick disk setup and optionally moves pagefile.
+
+.NOTES
+Author: 
+    DS
+Notes:
+    Revision 01
+Revision:
+    V01: 2025.01.08 by DS :: First version for GitHub.
+Call From:
+    Windows PowerShell v5.1 or newer.
+
+.INPUTS
+None
+
+.OUTPUTS
+None
+
+.PARAMETER Pagefile
+Move the pagefile to the newly setup disk volume.
+
+.EXAMPLE
+Invoke-QuickDiskSetup
+Initializes the largest RAW drive and formats it with the next available drive letter.
+
+.EXAMPLE
+Invoke-QuickDiskSetup -PageFile
+Initializes the largest RAW drive and formats it with the next available drive letter then moves the pagefile.
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [switch]$Pagefile = $false
+)
+
+# Subfunctions
+Function DriveLetter {
+	$script:letter = $null
+	68..90 | ForEach-Object { [char]$_ } | Foreach-Object {
+		if (!($script:letter)) {
+			if (Get-Volume -DriveLetter $_ -ErrorAction SilentlyContinue) {
+				Write-Verbose "$($_):\ already in use"
+			}
+			else {
+				$script:letter = $_
+			}
+		}
+	}
+}
+Function Initialize {
+	try {
+		$disk | Initialize-Disk -PartitionStyle GPT -ErrorAction Stop | Out-Null
+		$disk | New-Partition -UseMaximumSize -DriveLetter $script:letter -ErrorAction Stop | Out-Null
+		Format-Volume -DriveLetter $script:letter -FileSystem NTFS -Confirm:$false -ErrorAction Stop | Out-Null
+	}
+	catch {
+		throw
+	}
+}
+Function PageFile {
+	if ((Get-WmiObject -Class Win32_PageFileSetting).Name -ne "$($script:letter):\pagefile.sys") {
+		Write-Verbose "Move pagefile to $($script:letter):\"
+		
+		# Disable automatically managed pagefile
+		$SysInfo = Get-WmiObject -Class Win32_ComputerSystem -EnableAllPrivileges
+		$SysInfo.AutomaticManagedPageFile = $false
+		[Void]$sysinfo.Put()
+		
+		# Remove existing pagefile
+		(Get-WmiObject -Class Win32_PageFileSetting).Delete()
+
+		# New pagefile
+		$wmi = @{
+			'Class' = 'Win32_PageFileSetting'
+			'Arguments' = @{
+				name="$($script:letter):\pagefile.sys"
+			}
+			'EnableAllPrivileges' = $true
+		}
+		Set-WmiInstance @wmi | Out-Null
+	}
+	elseif ((Get-WmiObject -Class Win32_PageFileSetting).Name -eq "$($script:letter):\pagefile.sys") {
+		Write-Verbose "Pagefile already on $($script:letter):\"
+	}
+}
+
+# Determine disk
+$disk = Get-Disk | Where-Object {$_.PartitionStyle -eq 'RAW'} | Sort-Object Size -Descending | Select-Object -First 1
+
+if ($disk) {
+    DriveLetter
+    if ($script:letter) {
+        Initialize
+        if ($Pagefile) {
+			PageFile
+		}
+    }
+}
+else {
+    Write-Warning "No elligible disks found"
+}
+
+}
